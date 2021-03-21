ad48f7f98ef80a4ea974ff4e15bb0fcc3af6ebea
infret <infr3t@gmail.com>
2021-03-21 17:49:57 +0300

    Add users and messages

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <div id="app">
-    <div class="messages">
-      <div class="message" v-for="message in messages" :key="message">
-        <div class="bubble">{{ message }}</div>
+  <div class="app">
+    <nav class="navbar">
+      <div class="header">Chats</div>
+      <div class="messages">
+        <div class="header" v-for="chat in getChats()" :key="chat">
+          <div class="user">{{ chat.name }}</div>
+        </div>
       </div>
-    </div>
-    <form class="form" @submit.prevent="addMessage(value)">
-      <input class="input" type="text" v-model="value" />
-      <button class="button" type="submit"><img src="../resources/send.svg" /></button>
-    </form>
+    </nav>
+    <main>
+      <div class="header">{{ $store.state.users[$store.state.companionId].name }}</div>
+      <div class="messages">
+        <div class="message" v-for="message in getMessages()" :key="message">
+          <div
+            class="bubble"
+            :class="message.senderId === $store.state.currentId && 'bubble_current'"
+          >
+            {{ message.text }}
+          </div>
+        </div>
+      </div>
+      <form class="form" @submit.prevent="addMessage(value)">
+        <input class="input" type="text" v-model="value" placeholder="Enter message" />
+        <button class="button" type="submit"><img src="../resources/send.svg" /></button>
+      </form>
+    </main>
   </div>
 </template>
 
@@ -17,39 +33,101 @@ export default {
   name: 'App',
   components: {},
   data() {
-    return { messages: [] }
+    return {
+      value: ''
+    }
   },
   methods: {
     addMessage(text) {
-      this.messages.push(text)
+      this.$store.commit('addMessage', { text })
       this.value = ''
+    },
+    getMessages() {
+      let messages = []
+      this.$store.state.messages.map(
+        (message) =>
+          message.senderId === this.$store.state.currentId &&
+          message.receiverId === this.$store.state.companionId &&
+          messages.push(message)
+      )
+      this.$store.state.messages.map(
+        (message) =>
+          message.senderId === this.$store.state.companionId &&
+          message.receiverId === this.$store.state.currentId &&
+          messages.push(message)
+      )
+      return messages
+    },
+    getChats() {
+      let chats = []
+      this.$store.state.messages.map(
+        (message) =>
+          message.senderId === this.$store.state.currentId ||
+          (message.receiverId === this.$store.state.currentId &&
+            chats.push(this.$store.state.users[message.senderId]))
+      )
+      return chats
     }
   }
 }
 </script>
 
-<style scoped>
-#app {
-  max-width: 800px;
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  border: none;
+  outline: none;
+  box-sizing: border-box;
+  background: transparent;
+  font-size: 16px;
+  font-family: sans-serif;
+}
+
+.app {
+  max-width: 1000px;
   margin: 0 auto;
   height: 100vh;
   border-left: 1px solid gainsboro;
   border-right: 1px solid gainsboro;
+  display: flex;
+}
+
+.navbar {
+  width: 400px;
+  border-right: 1px solid gainsboro;
+}
+
+.chat {
+  height: 50px;
+}
+
+main {
+  height: 100%;
+  width: 100%;
+}
+
+.header {
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 10px;
 }
 
 .messages {
+  border-top: 1px solid gainsboro;
   border-bottom: 1px solid gainsboro;
-  width: 100%;
-  height: calc(100% - 50px);
-  overflow-x: auto;
+  height: calc(100% - 100px);
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column-reverse;
 }
 
 .message {
   width: 100%;
-  margin: 10px;
+  margin-bottom: 10px;
   font-size: 16px;
   display: flex;
-  justify-content: space-between;
   align-items: center;
 }
 
@@ -63,6 +141,11 @@ export default {
   white-space: pre-wrap;
 }
 
+.bubble_current {
+  margin-left: auto;
+  background-color: #f1f1f1;
+}
+
 .form {
   height: 50px;
   display: flex;
@@ -73,9 +156,10 @@ export default {
 .input {
   height: 100%;
   width: 100%;
-  padding: 12px;
+  padding: 10px;
 }
 
 .button {
+  margin: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './components/App.vue'
-
 Vue.config.productionTip = false
+Vue.use(Vuex)
 
 new Vue({
-  render: h => h(App),
+  store: new Vuex.Store({
+    state: {
+      currentId: 0,
+      companionId: 1,
+      users: [
+        { id: 0, name: 'infret' },
+        { id: 1, name: 'ryancooper' }
+      ],
+      messages: [
+        { senderId: 1, receiverId: 0, text: 'Hi' },
+        { senderId: 0, receiverId: 1, text: 'Sup' }
+      ]
+    },
+    mutations: {
+      addMessage(state, { text }) {
+        state.messages.push({ senderId: state.currentId, receiverId: state.companionId, text })
+      }
+    }
+  }),
+  render: (h) => h(App)
 }).$mount('#app')
